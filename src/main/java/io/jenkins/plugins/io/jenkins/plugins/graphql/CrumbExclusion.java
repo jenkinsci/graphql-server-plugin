42410feaa927406fee1ad69dc492fdf1c8f792e3
Gavin Mogan <gmogan@cloudbees.com>
2019-04-03 14:20:47 -0700

    editorconfig fixes

---
+++ src/main/java/io/jenkins/plugins/io/jenkins/plugins/graphql/CrumbExclusion.java
@@ -0,0 +1,25 @@
+package io.jenkins.plugins.io.jenkins.plugins.graphql;
+
+import hudson.Extension;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Extension
+@SuppressWarnings("unused")
+public class CrumbExclusion extends hudson.security.csrf.CrumbExclusion {
+
+    @Override
+    public boolean process(HttpServletRequest req, HttpServletResponse resp, FilterChain chain)
+            throws IOException, ServletException {
+        String pathInfo = req.getPathInfo();
+        if (pathInfo != null && pathInfo.startsWith("/graphql")) {
+            chain.doFilter(req, resp);
+            return true;
+        }
+        return false;
+    }
+}
