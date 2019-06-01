1e2bff451e155dd7e20c7a8a37eb22e960452907
Gavin Mogan <git@gavinmogan.com>
2019-05-31 17:39:37 -0700

    tests

--- src/test/java/io/jenkins/plugins/io/jenkins/plugins/graphql/BuildersTest.java
+++ src/test/java/io/jenkins/plugins/io/jenkins/plugins/graphql/BuildersTest.java
@@ -1,50 +1,28 @@
 package io.jenkins.plugins.io.jenkins.plugins.graphql;
 
+import com.google.common.base.Joiner;
 import graphql.ExecutionInput;
 import graphql.ExecutionResult;
 import graphql.GraphQL;
-import graphql.scalars.ExtendedScalars;
-import graphql.schema.*;
-import graphql.schema.idl.RuntimeWiring;
-import graphql.schema.idl.TypeRuntimeWiring;
+import graphql.schema.GraphQLFieldDefinition;
+import graphql.schema.GraphQLObjectType;
+import graphql.schema.GraphQLSchema;
 import hudson.model.FreeStyleBuild;
-import hudson.model.FreeStyleProject;
 import hudson.model.Run;
+import net.sf.json.JSONObject;
 import org.junit.Test;
 import org.mockito.Mockito;
-import sun.util.resources.LocaleData;
 
 import java.io.IOException;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.GregorianCalendar;
+import java.util.List;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class BuildersTest {
-
     @Test
-    public void buildSchemaFromClass() {
-//        Builders b = new Builders();
-//        GraphQLObjectType graphQLObjectType = b.buildSchemaFromClass(FreeStyleProject.class);
-//        assertEquals(
-//            "GraphQLObjectType{name='FreeStyleProject', description='moo', fieldDefinitionsByName=[_class, actions, allBuilds, buildable, builds, color, concurrentBuild, description, displayName, displayNameOrNull, downstreamProjects, firstBuild, fullDisplayName, fullName, healthReport, inQueue, keepDependencies, labelExpression, lastBuild, lastCompletedBuild, lastFailedBuild, lastStableBuild, lastSuccessfulBuild, lastUnstableBuild, lastUnsuccessfulBuild, name, nextBuildNumber, property, queueItem, scm, upstreamProjects, url], interfaces=[]}",
-//            graphQLObjectType.toString()
-//        );
-    }
-
-    @Test
-    public void buildSchema() {
-        Builders b = new Builders();
-        assertEquals(
-            "GraphQLObjectType{name='FreeStyleProject', description='moo', fieldDefinitionsByName=[_class, actions, allBuilds, buildable, builds, color, concurrentBuild, description, displayName, displayNameOrNull, downstreamProjects, firstBuild, fullDisplayName, fullName, healthReport, inQueue, keepDependencies, labelExpression, lastBuild, lastCompletedBuild, lastFailedBuild, lastStableBuild, lastSuccessfulBuild, lastUnstableBuild, lastUnsuccessfulBuild, name, nextBuildNumber, property, queueItem, scm, upstreamProjects, url], interfaces=[]}",
-            b.buildSchema().toString()
-        );
-    }
-
-    @Test
-    public void produceExtendedScalars() throws IOException {
+    public void timestampAsRFC() throws IOException {
         Builders b = new Builders();
         GregorianCalendar c = new GregorianCalendar();
         c.setTimeInMillis(1559344577604L);
@@ -54,30 +32,29 @@ public class BuildersTest {
 
         GraphQLObjectType graphqlRun = (GraphQLObjectType) b.buildSchema().getType("Run");
 
-        assertEquals(
-            "GraphQLObjectType{name='Run', description='moo', fieldDefinitionsByName=[_class, actions, artifacts, building, description, displayName, duration, estimatedDuration, executor, fingerprint, fullDisplayName, id, keepLog, number, queueId, result, timestamp, url], interfaces=[]}",
-            graphqlRun.toString()
-        );
+        ExecutionResult executeResult = _queryDataSet(run, graphqlRun, Arrays.asList("timestamp"));
 
+        assertEquals(
+            JSONObject.fromObject("{\"test\": {\"timestamp\": \"2019-05-31T16:16:17.604-07:00\"}}"),
+            JSONObject.fromObject(executeResult.getData())
+        );
+    }
+
+    private ExecutionResult _queryDataSet(Object data, GraphQLObjectType graphqlRun, List<String> fields) {
         GraphQLSchema builtSchema = GraphQLSchema.newSchema()
             .query(
                 GraphQLObjectType.newObject().name("QueryType").field(GraphQLFieldDefinition.newFieldDefinition()
                     .name("test")
                     .type(graphqlRun)
-                    .dataFetcher(environment -> run)
+                    .dataFetcher(environment -> data)
                     .build()
                 ).build()
             )
             .additionalType(graphqlRun)
             .build();
 
-        ExecutionInput executionInput = ExecutionInput.newExecutionInput().query("query { test { timestamp } }").build();
-        ExecutionResult executeResult = GraphQL.newGraphQL(builtSchema).build().execute(executionInput);
-
-        assertEquals(
-            "{test={timestamp=2019-05-31T16:16:17.604-07:00}}",
-            executeResult.getData()
-        );
+        ExecutionInput executionInput = ExecutionInput.newExecutionInput().query("query { test { " + Joiner.on(";").join(fields) + " } }").build();
+        return GraphQL.newGraphQL(builtSchema).build().execute(executionInput);
     }
 
 }
