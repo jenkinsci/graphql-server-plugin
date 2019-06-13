b92468135fda0cfa11d76a53c8de08bf33fca420
Gavin Mogan <git@gavinmogan.com>
2019-06-12 17:15:58 -0700

    sonarqube lint fixes

--- src/main/java/io/jenkins/plugins/graphql/IdFinder.java
+++ src/main/java/io/jenkins/plugins/graphql/IdFinder.java
@@ -7,16 +7,18 @@ import java.lang.reflect.Modifier;
 import java.util.Set;
 
 public class IdFinder {
+    private IdFinder() { }
+
     public static Method idMethod(Class<?> clazz) {
 
         Set<Method> getters = ReflectionUtils.getAllMethods(clazz,
             ReflectionUtils.withModifier(Modifier.PUBLIC), ReflectionUtils.withPrefix("get"));
 
         for (Method getter : getters) {
-            if ("getid".equals(getter.getName().toLowerCase())) {
+            if ("getid".equalsIgnoreCase(getter.getName())) {
                 return getter;
             }
-            if ("getfullname".equals(getter.getName().toLowerCase())) {
+            if ("getfullname".equalsIgnoreCase(getter.getName())) {
                 return getter;
             }
         }
